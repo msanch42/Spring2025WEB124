aa170c08898406c1987195deb096018442463565
Monica <msanch42@stumail.jccc.edu>
2025-03-05 11:48:29 -0600

    Create project2.js

---
+++ Chapter2Project/project2.js
@@ -0,0 +1,5 @@
+let myName = "Monica Sanchez"; //create and initialize a string variable named myName with your full name
+
+let para1 = document.getElementById("p1"); //create and initialize a variable named para1 and set it equal to document.getElementById("p1")
+
+para1.textContent = myName; //set para1.textContent = myName
