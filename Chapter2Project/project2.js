afd598680e36eaf0cfc3274839e2211207834ecd
Monica <msanch42@stumail.jccc.edu>
2025-03-08 16:41:57 -0600

    Update project2.js

--- Chapter2Project/project2.js
+++ Chapter2Project/project2.js
@@ -3,3 +3,12 @@ let myName = "Monica Sanchez"; //create and initialize a string variable named m
 let para1 = document.getElementById("p1"); //create and initialize a variable named para1 and set it equal to document.getElementById("p1")
 
 para1.textContent = myName; //set para1.textContent = myName
+
+let n1 = 28; //create and initialize two number variables with the names n1 and n2 using any numeric values desired
+
+let n2 = 36;
+
+let numberSum = n1 + n2; //create and initialize a variable named numberSum to add the two numbers using their variable names
+
+document.GetElementById("p2").textContent = numberSum; //set document.getElementById("p2").textContent = numberSum;
+
