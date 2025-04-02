5b27b916e1eb0a701556d095710eed1e9d264a2d
Monica <msanch42@stumail.jccc.edu>
2025-04-02 16:08:26 -0500

    Create script.js

---
+++ Chapter3Project/script.js
@@ -0,0 +1,29 @@
+//Monica Sanchez 4-2-25
+
+// Declare a variable to store your full name
+let fullName = "Monica Sanchez";
+console.log("Full Name:", fullName);
+
+//Declare a variable to store your desired annual salary
+let desiredSalary = 150000;
+console.log("My Desired Annual Salary:", desiredSalary);
+
+//Declare a variable to store your veteran status (hint: either you are a veteran or you are not a veteran)
+let isVeteran = true;
+console.log("Are you a JavaScript veteran?:", isVeteran);
+
+//Delcare an array to store the names of three of your friends. 
+let myFriends = ["Ashley", "Armani", "Lona"];
+console.log("My Friends Are:", myFriends);
+
+//Delcare an array to store the value of the desired annual salary for your three friends
+let friendsSalaries = [90000, 200000, 120000];
+console.log("My Friends' Desired Salaries:", friendsSalaries);
+
+//Declare a literal object to store the first name, last name, and desired annual salary of yet another friend
+let anotherFriend = {
+  firstName: 'Cinnamon',
+  lastName: 'Roll',
+  desiredSalary: 123456
+};
+console.log("Here's My Other Friend's Info:", anotherFriend);
