20bfa6e7488a6c31da9503ee465107ca64b340c7
Monica <msanch42@stumail.jccc.edu>
2025-05-01 21:56:55 -0500

    Update script.js

--- FinalExam/script.js
+++ FinalExam/script.js
@@ -1,51 +1,102 @@
 //Monica Sanchez 5-1-25
 //Created JavaScript functions to calculate given number
-function doAddition(num){
+/*function calculateAddition(num){
   let result = "";
   for (let i = 1; i <= 10; i++) {
       result += `${num} + ${i} = ${num + i}<br>`;
   }
-  document.getElementById("addition").innerHTML = result; 
+  document.getElementById('addition').innerHTML = result; 
 }
 
-function doSubtraction(num){
+function calculateSubtraction(num){
   let result = "";
   let i = 1;
   while (i <= 10) {
       result += `${num} - ${i} = ${num - i}<br>`;
       i++;
   } 
-  document.getElementById("subtraction").innerHTML = result; 
+  document.getElementById('subtraction').innerHTML = result; 
 }
 
-function doMultiplication(num){
+function calculateMultiplication(num){
   let result = "";
   let i = 1;
   do {
       result += `${num} * ${i} = ${num * i}<br>`;
       i++;
   } while(i <= 10);
-  document.getElementById("multiplication").innerHTML = result; 
+  document.getElementById('multiplication').innerHTML = result; 
 }
 
-function doDivision(num){
+function calculateDivision(num){
   let result = "";
   for (let i = 1; i <= 10; i++) {
     result += `${num} / ${i} = ${(num / i).toFixed(2)}<br>` //return only two digits to the right of the decimal point in the division function
   }
-  document.getElementById("division").innerHTML = result; 
+  document.getElementById('division').innerHTML = result; 
 }
 
 function calculateAll() {
   const num = parseFloat(document.getElementById("num").value);
   if(!isNan(num)) {
-    doAddition(num);
-    doSubtraction(num);
-    doMultiplication(num);
-    doDivision(num);
+    calculateAddition(num);
+    calculateSubtraction(num);
+    calculateMultiplication(num);
+    calculateDivision(num);
   } else {
     alert("PLEASE ENTER A VALID NUMBER!");
   }
 }
 
-document.getElementById("calculate").addEventListener("click", calculateAll);
+document.getElementById("calculate').addEventListener("click", calculateAll);*/
+    function calculateAddition(value) {
+      let result = '';
+      for (let i = 1; i <= 10; i++) {
+        result += `${value} + ${i} = ${value + i}<br>`;
+      }
+      document.getElementById('addition').innerHTML = result;
+    }
+
+    function calculateSubtraction(value) {
+      let result = '';
+      let i = 1;
+      while (i <= 10) {
+        result += `${value} - ${i} = ${value - i}<br>`;
+        i++;
+      }
+      document.getElementById('subtraction').innerHTML = result;
+    }
+
+    function calculateMultiplication(value) {
+      let result = '';
+      let i = 1;
+      do {
+        result += `${value} * ${i} = ${value * i}<br>`;
+        i++;
+      } while (i <= 10);
+      document.getElementById('multiplication').innerHTML = result;
+    }
+
+    function calculateDivision(value) {
+      let result = '';
+      for (let i = 1; i <= 10; i++) {
+        result += `${value} / ${i} = ${(value / i).toFixed(2)}<br>`;
+      }
+      document.getElementById('division').innerHTML = result;
+    }
+
+    function runAllCalculations() {
+      const inputVal = parseFloat(document.getElementById('num').value);
+      if (isNaN(inputVal)) {
+        alert("Please enter a valid number.");
+        return;
+      }
+
+      calculateAddition(inputVal);
+      calculateSubtraction(inputVal);
+      calculateMultiplication(inputVal);
+      calculateDivision(inputVal);
+    }
+
+    document.getElementById('calcBtn').addEventListener('click', runAllCalculations);
+
